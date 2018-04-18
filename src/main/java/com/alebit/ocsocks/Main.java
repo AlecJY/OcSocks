e63d221d1b8b4b9cbc04a90a8dacf0c6096f9908
AlecJY <ae40515@yahoo.com.tw>
2018-04-18 18:47:15 +0800

    Change sockslib from binary to submodule

--- src/main/java/com/alebit/ocsocks/Main.java
+++ src/main/java/com/alebit/ocsocks/Main.java
@@ -1,14 +1,13 @@
 package com.alebit.ocsocks;
 
-import fucksocks.server.GenericSocksProxyServer;
-import fucksocks.server.SocksProxyServer;
-import fucksocks.server.SocksProxyServerFactory;
+import sockslib.server.SocksProxyServer;
+import sockslib.server.SocksProxyServerFactory;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        SocksProxyServer socksProxyServer = SocksProxyServerFactory.newNoAuthenticaionServer();
+        SocksProxyServer socksProxyServer = SocksProxyServerFactory.newNoAuthenticationServer(8080);
         try {
             socksProxyServer.start();
         } catch (IOException e) {
